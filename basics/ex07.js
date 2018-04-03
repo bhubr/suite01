99ea6b1055f15adf79f2ce3f966250e77219ccdc
Leila DQIQI <leila.dqiqi@yahoo.fr>
2018-04-03 15:31:20 +0200

    réponse de l'exo 07

--- basics/ex07.js
+++ basics/ex07.js
@@ -7,18 +7,26 @@
 
 //  écrire votre code sous ce commentaire
 
+function boostedAddition(nb) {
+ let somme = 0;
+ for (let i=0; i < nb.length; i++) { 
+ somme += nb[i]
+
+}
+return somme
+}
 /*
   Test 1
   Résultat attendu : 15
 */
 
-boostedAddition([4, 5, 6]);
+console.log(boostedAddition([4, 5, 6]));
 
 /*
   Test 2
   Résultat attendu : 44
 */
-boostedAddition([4, 4, 6, 8, 10, 12]);
+console.log(boostedAddition([4, 4, 6, 8, 10, 12]));
 
 /* DO NOT TOUCH */
 module.exports = {
